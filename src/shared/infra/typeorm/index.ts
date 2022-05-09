7d5e3d5eb5c2e79681f493df7c7adcb127d3324a
pdr3392 <pchenr1que1@gmail.com>
2022-05-09 00:40:33 -0300

    database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,9 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database") => {
+export default async () => {
   const options = await getConnectionOptions();
 
   Object.assign(options, {
-    host: process.env.NODE_ENV === "test" ? "localhost" : host,
     database: process.env.NODE_ENV === "test" ? "rentalx_test" : "rentalx",
   });
 
