dbcc93947c5963f2c4477d32dd9df1cf9b6b2d52
pdr3392 <pchenr1que1@gmail.com>
2022-05-11 03:37:49 -0300

    sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,8 @@ import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { AppError } from "@shared/errors/AppError";
 import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import { router } from "@shared/infra/http/routes";
@@ -22,6 +24,19 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -32,6 +47,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
